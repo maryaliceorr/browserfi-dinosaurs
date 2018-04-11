97d3c796c1d1fb90e4b69c30c524e7f089db42e2
Mary Alice Orr <m.a.orr17@gmail.com>
2018-04-10 21:43:49 -0500

    added dino names

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,12 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const monkeyButt = require('./dinosaurs');
+const printToDom = require('./dom');
 
 let dinoArray = [];
 
 const whenDinosLoad = function () {
   dinoArray = JSON.parse(this.responseText).dinosaurs;
-  console.log('dinoArray', dinoArray);
+  printToDom(dinoArray);
 };
 
 const badDinos = function () {
@@ -25,7 +26,7 @@ module.exports = {
   getDinos,
 };
 
-},{"./dinosaurs":2}],2:[function(require,module,exports){
+},{"./dinosaurs":2,"./dom":3}],2:[function(require,module,exports){
 const loadDinos = (loadFunction, errorFunction) => {
   const myRequest = new XMLHttpRequest();
   myRequest.addEventListener('load', loadFunction);
@@ -37,8 +38,30 @@ const loadDinos = (loadFunction, errorFunction) => {
 module.exports = loadDinos;
 
 },{}],3:[function(require,module,exports){
+const domEvents = require('./events');
+
+const outputDiv = document.getElementById('dinos');
+
+const domString = (dinos) => {
+  let strang = '';
+  dinos.forEach((dino) => {
+    strang += `<h2>${dino.type}</h2>`;
+  });
+  return strang;
+};
+
+const printToDom = (dinoArray) => {
+  outputDiv.innerHTML = domString(dinoArray);
+  domEvents();
+};
+
+module.exports = printToDom;
+
+},{"./events":4}],4:[function(require,module,exports){
+
+},{}],5:[function(require,module,exports){
 const data = require('./data');
 
 data.initializer();
 
-},{"./data":1}]},{},[3]);
+},{"./data":1}]},{},[5]);
